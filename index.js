fee4731119f4131d5fa97053f752f149991a1954
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-07 20:26:19 +0600

    feat(delete): delete method work successfully to delete cart item from database

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5002;
 app.use(express.json());
 app.use(cors());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.40hja.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -50,6 +50,13 @@ async function run() {
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
       console.log(result);
+      res.json(result);
+    });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.json(result);
     });
 
