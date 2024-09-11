027447cb70a90aefa4e71ecab0e13d190c824bf8
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-11 06:22:20 +0600

    feat(patch): generate patch route to update mentu items

--- index.js
+++ index.js
@@ -148,6 +148,23 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/menu/:id", async (req, res) => {
+      const item = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          name: item.name,
+          category: item.category,
+          price: item.price,
+          recipe: item.recipe,
+          image: item.image,
+        },
+      };
+      const result = await menuCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     //reviews routes
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray();
