7ab8b521ad5e868518806c41d5a28ccd39bc35bd
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-11 16:35:58 +0600

    feat(paymentIntent): create payment-intent for payment gate way of stripe

--- index.js
+++ index.js
@@ -193,6 +193,20 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
