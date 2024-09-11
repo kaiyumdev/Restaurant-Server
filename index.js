722ac755dadab50561ca7b0679a21b031649a6f8
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-11 21:43:08 +0600

    feat(payments): create payments method & delete item after payment from database

--- index.js
+++ index.js
@@ -213,7 +213,13 @@ async function run() {
       const payment = req.body;
       const paymentResult = await paymentCollection.insertOne(payment);
       console.log("Payment Received: ", payment);
-      res.send(paymentResult);
+      const query = {
+        _id: {
+          $in: payment.cartIds.map((id) => new ObjectId(id)),
+        },
+      };
+      const deleteResult = await cartCollection.deleteMany(query);
+      res.send({ paymentResult, deleteResult });
     });
 
     console.log(
