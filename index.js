a76f63c2feeee0ceb40f7d101cbd7464c2f92cce
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-11 17:41:30 +0600

    feat(amount): successfully got total amount of order price

--- index.js
+++ index.js
@@ -196,6 +196,7 @@ async function run() {
     app.post("/create-payment-intent", async (req, res) => {
       const { price } = req.body;
       const amount = parseInt(price * 100);
+      console.log("amount inside the intent: " + amount);
 
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
