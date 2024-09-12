bcaa4e70841989a6cfe372e5bcf8d69803045fcc
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-12 12:25:48 +0600

    (payments): create payments get route to get payment data by email query

--- index.js
+++ index.js
@@ -209,6 +209,15 @@ async function run() {
       });
     });
 
+    app.get("/payments/:email", verityToken, async (req, res) => {
+      const query = { email: req.params.email };
+      if (req.params.email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const result = await paymentCollection.findOne(query).toArray();
+      res.send(result);
+    });
+
     app.post("/payments", async (req, res) => {
       const payment = req.body;
       const paymentResult = await paymentCollection.insertOne(payment);
