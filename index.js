1b1344b6e65c048140bfa773807b0deaed517474
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-14 00:47:22 +0600

    feat(aggreagate): get total revenue by using aggreagate pipeline

--- index.js
+++ index.js
@@ -238,11 +238,25 @@ async function run() {
       const orders = await paymentCollection.estimatedDocumentCount();
 
       //this is not the best way
-      const payments = await paymentCollection.find.toArray();
-      const revenue = payments.reduce(
-        (total, payment) => total + payment.price,
-        0
-      );
+      // const payments = await paymentCollection.find.toArray();
+      // const revenue = payments.reduce(
+      //   (total, payment) => total + payment.price,
+      //   0
+      // );
+
+      const result = await paymentCollection
+        .aggregate([
+          {
+            $group: {
+              _id: null,
+              totalRevenue: {
+                $sum: "$price",
+              },
+            },
+          },
+        ])
+        .toArray();
+      const revenue = result.length > 0 ? result[0] : 0;
       res.send({ users, menuItems, orders, revenue });
     });
 
